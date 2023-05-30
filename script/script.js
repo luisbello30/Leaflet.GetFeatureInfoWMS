a897e9d7657bc6d8b8e7ebc6e8e336727fb24a56
Luis Bello <luisbello25@icloud.com>
2023-05-30 19:44:05 +0200

    Update .eslintrc.js, index.html, package-lock.json, and 3 more files

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
+/* global L */
 
-const data = L.map("map1").setView([40.404610346549035, -4.25039310600574], 6);
+const map = L.map('map1').setView([40.404610346549035, -4.25039310600574], 6);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
   attribution:
-    ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(data);
-
-
-
+    ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
