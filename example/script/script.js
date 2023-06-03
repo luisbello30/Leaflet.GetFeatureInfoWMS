52ec212c76f759f6cdbcfa52b3fc37251eb4f618
Luis Bello <luisbello25@icloud.com>
2023-06-03 19:53:44 +0200

    Example

--- example/script/script.js
+++ example/script/script.js
@@ -1,9 +1,26 @@
-/* global L */
-
-const map = L.map('map1').setView([40.404610346549035, -4.25039310600574], 6);
+const map = L.map('map1').setView([57.76, 11.96], 12);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-  attribution:
+    maxZoom: 18,
+    attribution:
     ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+
+const callBack = async function (url) {
+    await fetch(url)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+        });
+};
+
+L.tileLayer.getFeatureInfoWMS('https://public-mapservice.lf.goteborg.se/geoserver/LF_Externwebb/wms?', {
+    layers: 'Utrustning',
+    format: 'image/png',
+    transparent: true,
+    attribution: 'Public Geoserver LF Goteborg City',
+    // eslint-disable-next-line camelcase
+    feature_count: 2,
+    callBack
 }).addTo(map);
