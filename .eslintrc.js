23d36da44545ecf5263486fff7672c55f5416a75
Luis Bello <luisbello25@icloud.com>
2023-06-03 19:53:21 +0200

    Lint & ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,38 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'mourner',
+    overrides: [
+    ],
+    plugins: [
+        'eslint-plugin-import'
+    ],
+    globals: {
+        globalThis: true,
+        L: true
+    },
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'consistent-return': 'off',
+        'curly': 'error',
+        'import/extensions': ['error', 'ignorePackages'],
+        'indent': ['error', 4, {VariableDeclarator: 0, flatTernaryExpressions: true}],
+        'key-spacing': 'off',
+        'linebreak-style': ['off', 'unix'],
+        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+        'no-unused-expressions': ['error', {allowShortCircuit: true}],
+        'spaced-comment': 'error',
+        'strict': 'off',
+        'wrap-iife': 'off',
+        'guard-for-in': 'error',
+        // TODO: Re-enable the rules below and fix the linting issues.
+        'no-invalid-this': 'off',
+        'prefer-object-has-own': 'error',
+        'prefer-spread': 'off'
+    },
 };
