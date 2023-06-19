bcebf0efda21d13a2b1f489764dc98f1ab9e5712
Luis Bello <luis.bello@brightcomms.com>
2023-06-19 23:33:07 +0200

    update demo with 1.0.15

--- script/script.js
+++ script/script.js
@@ -3,14 +3,39 @@ const map = L.map('map1').setView([57.76, 11.96], 12);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution:
-    ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-const callBack = async function (url) {
-    await fetch(url)
+function tableData(data) {
+    let title = document.getElementById('header')
+    let detail = document.getElementById('detail')
+
+    title.replaceChildren()
+    detail.replaceChildren()
+
+    for (const i in data) {
+        if (data[i].properties) {
+            let row = ''
+            for (const prop in data[i].properties) {
+                if (i === '0') {
+                    title.innerHTML += `<th>${prop}</th>`
+                }
+                row += `<td>${data[i].properties[prop]}</td>`
+            }
+            detail.innerHTML += `<tr>${row}</tr>`
+        }
+    }
+}
+
+const callBack = function (url, params, point) {
+    console.log(url)
+    console.log(params)
+    console.log(point)
+    fetch(url)
         .then(response => response.json())
         .then((response) => {
             console.log(response);
+            tableData(response.features)
         });
 };
 
