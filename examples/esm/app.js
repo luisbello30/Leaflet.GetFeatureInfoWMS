7fa15e77d750e11e6428dedcdb51709c64800ce2
Luis Bello <luis.bello@brightcomms.com>
2023-07-05 02:24:21 +0200

    Update Example esm

--- examples/esm/app.js
+++ examples/esm/app.js
@@ -4,51 +4,51 @@ import { TileLayer } from 'leaflet-infowms';
 // import { TileLayer } from '../../dist/Leaflet.InfoWMS.esm';
 
 const map = new Map('mapa');
-const osmUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-const osm = new TileLayer(osmUrl, {
+const baseLayerUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const baseLayer = new TileLayer(baseLayerUrl, {
     attribution:
         ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 });
 
-const nexrad = new TileLayer.InfoWMS('https://public-mapservice.lf.goteborg.se/geoserver/LF_Externwebb/wms?', {
-    layers: 'Utrustning',
-    format: 'image/png',
-    transparent: true,
-    attribution: 'Public Geoserver LF Goteborg City',
-    feature_count: 1
-});
+const wmsLayerUrl = 'https://public-mapservice.lf.goteborg.se/geoserver/LF_Externwebb/wms?'
+const wmsLayer = new TileLayer.InfoWMS(wmsLayerUrl, {
+        layers: 'Utrustning',
+        format: 'image/png',
+        transparent: true,
+        attribution: 'Public Geoserver LF Goteborg City',
+        feature_count: 1
+    }
+);
 
-nexrad.on('click', function(e) {
-    console.log(e)
+wmsLayer.on('click', function (e) {
+    console.log(e);
     fetch(e.url)
-        .then(response => response.json())
+        .then((response) => response.json())
         .then((response) => {
             console.log(response);
-            tableData(response.features)
+            tableData(response.features);
         });
-})
+});
 
 function tableData(data) {
-    let title = document.getElementById('header')
-    let detail = document.getElementById('detail')
+    let title = document.getElementById('header');
+    let detail = document.getElementById('detail');
 
-    title.replaceChildren()
-    detail.replaceChildren()
+    title.replaceChildren();
+    detail.replaceChildren();
 
     for (const i in data) {
         if (data[i].properties) {
-            let row = ''
+            let row = '';
             for (const prop in data[i].properties) {
                 if (i === '0') {
-                    title.innerHTML += `<th>${prop}</th>`
+                    title.innerHTML += `<th>${prop}</th>`;
                 }
-                row += `<td>${data[i].properties[prop]}</td>`
+                row += `<td>${data[i].properties[prop]}</td>`;
             }
-            detail.innerHTML += `<tr>${row}</tr>`
+            detail.innerHTML += `<tr>${row}</tr>`;
         }
     }
 }
 
-map.addLayer(osm)
-    .addLayer(nexrad)
-    .setView([57.76, 11.96], 12);
+map.addLayer(baseLayer).addLayer(wmsLayer).setView([57.76, 11.96], 12);
